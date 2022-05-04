9d474ce974e86046021b10fa02605ba015a91268
Taida Kadrić <tkadric1@etf.unsa.ba>
2022-05-05 00:36:57 +0200

    first commit

---
+++ src/test/java/etf/unsa/ba/SpringCloudGateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package etf.unsa.ba.SpringCloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
