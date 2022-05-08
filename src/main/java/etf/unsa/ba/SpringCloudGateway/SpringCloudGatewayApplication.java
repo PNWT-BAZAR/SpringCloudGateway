b5891d871bd554c5fed635fdebbde06178f68fc6
Taida Kadrić <tkadric1@etf.unsa.ba>
2022-05-08 23:12:24 +0200

    Downstreaming requests completed

--- src/main/java/etf/unsa/ba/SpringCloudGateway/SpringCloudGatewayApplication.java
+++ src/main/java/etf/unsa/ba/SpringCloudGateway/SpringCloudGatewayApplication.java
@@ -2,8 +2,10 @@ package etf.unsa.ba.SpringCloudGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringCloudGatewayApplication {
 
 	public static void main(String[] args) {
