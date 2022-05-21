50e2ea831c75eee3927fcd90d09037505c3080b6
Taida Kadrić <tkadric1@etf.unsa.ba>
2022-05-21 13:45:41 +0200

    Security not finished

---
+++ src/main/java/etf/unsa/ba/SpringCloudGateway/Security/JwtConfig.java
@@ -0,0 +1,26 @@
+package etf.unsa.ba.SpringCloudGateway.Security;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class JwtConfig {
+    @Value("${security.jwt.uri:/identity/users/login/**}")
+    private String Uri;
+
+    @Value("${security.jwt.header:Authorization}")
+    private String header;
+
+    @Value("${security.jwt.prefix:Bearer }")
+    private String prefix;
+
+    @Value("${security.jwt.expiration:#{24*60*60}}")
+    private int expiration;
+
+    @Value("${security.jwt.secret:JwtSecretKey}")
+    private String secret;
+}
